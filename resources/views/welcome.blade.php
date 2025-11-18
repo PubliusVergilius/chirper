6536224ad15cf531201678fed33d2459c3b9475b
M. Vinícius <vinteesetedemaiodedoismil@gmail.com>
2025-11-18 01:37:50 -0500

    Add chirp controller

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,5 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getlocale()) }}">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
